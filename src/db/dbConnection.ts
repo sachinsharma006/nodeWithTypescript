ca63ced7ee1ec232fb94a4f08698b8ba83d3d10a
Sachin Sharma <sa.sharma6@gemsol.in>
2022-07-27 16:32:38 +0530

    added the .env file

--- src/db/dbConnection.ts
+++ src/db/dbConnection.ts
@@ -3,16 +3,16 @@ import { MongoClient } from "mongodb";
 var collection:any;
 const connectToDB = async () => {
   const DB_URL: string = String(process.env.DB_URL);
+  const DB_NAME: string = String(process.env.DB_NAME);
+  const DB_COLLECTION: string = String(process.env.DB_COLLECTION);
   console.log(typeof process.env.DB_URL)
   console.log(DB_URL)
   try {
-    const URL =
-      "mongodb+srv://sachin006:sachin006@cluster0.hel6j.mongodb.net/?retryWrites=true&w=majority";
 
-    const client = new MongoClient(URL);
+    const client = new MongoClient(DB_URL);
     await client.connect();
-    const db = client.db("TypeScriptTasks");
-    collection = db.collection("Tasks");
+    const db = client.db(DB_NAME);
+    collection = db.collection(DB_COLLECTION);
     console.log("DB Connected")
   } catch (err) {
     console.log(err);
